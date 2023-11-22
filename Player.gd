8d44c9c488264e73eaa44139edb46ab6dc32dbed
Shravan <shravanshetty322@gmail.com>
2023-11-22 19:27:00 +0530

    feat: optimised animation implementation using animation trees

--- Player.gd
+++ Player.gd
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var spring_arm: SpringArm3D = $SpringArm3D
 @onready var camera: Camera3D = $SpringArm3D/Camera3D
 @onready var pivot: Node3D = $Pivot  
-@onready var animation_player: AnimationPlayer = $Pivot/Body/AnimationPlayer  
+@onready var animation_tree: AnimationTree = $Pivot/Body/AnimationTree 
 
 
 func _physics_process(delta):
@@ -19,8 +19,8 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,10 +32,7 @@ func _physics_process(delta):
 	# we can probably remove this now
 	direction = direction.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
 	
-	
-	
 	if direction:
-		
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -44,20 +41,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	# use look at instead
 	if velocity.length() > 0.2 :
 		pivot.look_at(Vector3(camera.global_position.x, 0, camera.global_position.z))
-	#	var look_direction = Vector2(velocity.z, velocity.x)
-	#	pivot.rotation.y = lerp_angle(pivot.rotation.y, look_direction.angle(), delta*SPEED*5)
 		
-	if Input.is_action_pressed("ui_up") :
-		animation_player.play("run_forward")
-	elif Input.is_action_pressed("ui_down") :
-		animation_player.play("run_backward")
-	else:	
-		if Input.is_action_pressed("ui_left") :
-			animation_player.play("strafe_left")
-		elif Input.is_action_pressed("ui_right") :
-			animation_player.play("strafe_right")
-		else:	
-			animation_player.play("idle")
+	animation_tree.set("parameters/movement/blend_position", Vector2(input_dir.x, input_dir.y))
