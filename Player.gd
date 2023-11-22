302aad12e3fc4867234446697e52c26362f15421
Shravan <shravanshetty322@gmail.com>
2023-11-23 00:04:31 +0530

    feat added attack + bugged roll

--- Player.gd
+++ Player.gd
@@ -13,6 +13,14 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var animation_tree: AnimationTree = $Pivot/Body/AnimationTree 
 
 
+func _unhandled_input(event):
+	var playback = animation_tree.get("parameters/playback")
+	if Input.is_action_just_pressed("ui_accept"):
+		playback.travel("roll")
+	if Input.is_action_just_pressed("attack"):
+		playback.travel("attack")
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -27,6 +35,10 @@ func _physics_process(delta):
 	
 	
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
+	var playback = animation_tree.get("parameters/playback")
+	if playback.get_current_node() != "movement":
+		input_dir = Vector2.ZERO	
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# we can probably remove this now
@@ -38,10 +50,20 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)	
+		
+	if playback.get_current_node() == "roll":
+		velocity.x = pivot.global_transform.basis.z.x * SPEED
+		velocity.z = pivot.global_transform.basis.z.z * SPEED
 
 	move_and_slide()
 	
-	if velocity.length() > 0.2 :
+	if input_dir != Vector2.ZERO :
 		pivot.look_at(Vector3(camera.global_position.x, 0, camera.global_position.z))
 		
-	animation_tree.set("parameters/movement/blend_position", Vector2(input_dir.x, input_dir.y))
+	animation_tree.set("parameters/movement/blend_position", Vector2(input_dir.x, -input_dir.y))
+
+		
+
+	
+	
+
